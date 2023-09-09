7b44488b1f27421f30109acdd72b47b741b2319d
Ivan Mutsko <imucko1996@gmail.com>
2023-09-09 17:19:42 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { HeadNav } from '../HeadNav/HeadNav';
-import { Loader } from '../Loader/Loader';
 import { Container } from './Layout.styled';
 
 export const Layout = () => {
   return (
     <Container>
       <HeadNav />
-      <Suspense fallback={Loader}>
+      <Suspense>
         <Outlet />
       </Suspense>
     </Container>
